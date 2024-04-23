26a466e463dea651d12b5b37a41a1d8874e54dbe
Rahul Singh Rawat <125528268+rahulrwt05@users.noreply.github.com>
2024-04-23 21:03:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (83.82%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans=1;int m=n;       if (n == Integer.MIN_VALUE) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == Integer.MIN_VALUE) {
             x *= x;
             n /= 2;
         }
-        if(n==0){
-            return 1;
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
         }
-        if(n<0){
-            n=-1*n;
+        double ans = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            n /= 2;
         }
-       while(n>0){
-if(n%2==1){ans=ans*x;
-n=n-1;}else{
-       n=n/2;x*=x;
-        }} if(m<0){
-            return 1.0/ans;
-        }return ans;   }
+        return ans;
+    }
 }
