339e0b4004b4c7e9afc00829a0a31e70e6300779
Rahul Singh Rawat <125528268+rahulrwt05@users.noreply.github.com>
2024-04-26 02:47:16 +0530

    Time: 91 ms (85.14%) | Memory: 50.4 MB (22.29%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -10,18 +10,20 @@ class Solution {
             }
         }
      }// 2 se right tak sieve arraye se primes no left se right ke bich me add kardiya ek array me
+     //math.max isliye kyuki left kahi kahi 1 de rakha
      for(int i=Math.max(2,left);i<=right;i++){
         if(sieve[i]==true){
             primes.add(i);
         }
-     }
+     }//now find two closest primes 
      int a=0;int b=Integer.MAX_VALUE;
      for(int j=1;j<primes.size();j++){
         if(primes.get(j)-primes.get(j-1)<(b-a)){
             b=primes.get(j);
             a=primes.get(j-1);
         }
-     }if(a==0){
+     }//exception case
+     if(a==0){
         return new int[]{-1,-1};
      }
     return new int[]{a,b};}
