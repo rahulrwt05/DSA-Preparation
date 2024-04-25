ede73a7c4ac878438b9f8c90138d92d1146536b7
Rahul Singh Rawat <125528268+rahulrwt05@users.noreply.github.com>
2024-04-25 21:05:35 +0530

    Time: 99 ms (59.54%) | Memory: 48.9 MB (65.94%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -3,14 +3,21 @@ class Solution {
         boolean arr[] =new boolean[n+1];
         // n+1 as it  will create an extra space for 0 index by shifting one ahea we will get i=2 for 2 value if not had n+1 we would get 3 
         int count=0;
+        // n= arr.length-1 =12 2 se 12 tak chalega 
         for(int i=2;i< n;i++){
             arr[i]=true;
-        }if(n<3){
+        }// exception case
+        if(n<3){
             return 0;
         }
-        for(int i=2;i*i<=n;i++){
+        // 2 se root tak n ke baki sb cover ho jayenge n=25, 2,3,4,5
+        // 2 ke table cover hojayenge 2 pahle se true fir ye sb false,4 ,6,8,10,12,14,16,18,20,22,24
+        // 3 ke cover hojayenge 3 pahle se , fir 9 , 12,15,18,21,24
+        // 4 false ho gya skip 
+        // 5 sidhe 25 kardega jiska koi kam ni
+        for(int i=2;i*i<n;i++){
             if(arr[i]==true){
-                for(int j=i*i;j<=n;j+=i){
+                for(int j=i*i;j<n;j+=i){
                     arr[j]=false;
                 }
             }
