de71c26469b1cdca04f66c4e3894fec529fcaf0c
Rahul Singh Rawat <125528268+rahulrwt05@users.noreply.github.com>
2024-04-26 16:47:44 +0530

    Time: 1 ms (86.17%) | Memory: 40.5 MB (76.14%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -4,12 +4,10 @@ class Solution {
         int end = x;
         int ans = 0;
         
-        if(x == 0 && x == 1){
-            return 1;
-        }
+
         while (start <= end) {
             int mid = start+ (end - start) / 2;
-
+//  Optimized Logic: You're using integer division x / mid to compare with mid instead of multiplying mid with itself, which could lead to integer overflow for large values of x. This makes your code more efficient and prevents overflow issues.
             if (x / mid == mid) {
                 ans = mid;
                 return ans;
@@ -20,6 +18,6 @@ class Solution {
                 start = mid + 1;
             }
         }
-        return end;
+        return ans;
     }
 }
