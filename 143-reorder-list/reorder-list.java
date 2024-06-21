c96be6d4638cc70df023d21e3f3a6659c54871fd
Rahul Singh Rawat <125528268+rahulrwt05@users.noreply.github.com>
2024-06-21 17:35:27 +0530

    Time: 2 ms (82.96%) | Memory: 47.9 MB (68.34%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -24,8 +24,8 @@ ListNode temp=head;
         }
     }static ListNode findmid(ListNode head){
         ListNode slow=head;
-        ListNode fast=head;
-        while(fast!=null && fast.next!=null && fast.next.next != null ){
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null ){
             slow=slow.next;
             fast=fast.next.next;
         }
