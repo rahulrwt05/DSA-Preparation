908301ee1a1a66289aa5800862b14f20b6841d9a
Rahul Singh Rawat <125528268+rahulrwt05@users.noreply.github.com>
2024-06-29 16:02:00 +0530

    Time: 1 ms (100.00%) | Memory: 58.1 MB (68.22%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -1,54 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-
-        if(head==null || head.next==null) return head;
-
-        ListNode len = head;
-        int length = 0;
-
-        // calculate length
-        while(len!=null){
-            len = len.next;
-            length++;
+        ListNode fast=head,slow=head,f=null;
+        for(int i=1;i<k;i++) fast=fast.next;
+        f=fast;
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
         }
-       
-
-        ListNode curr1 = head;
-        ListNode curr2 = head;
-
-        int l = length-k;
-
-        //finding left node
-        while(k-->1){
-            curr1 = curr1.next;
-        }
-        
-        // finding right node
-        while(l-->0){
-            curr2 = curr2.next;
-        }
-        
-        // edge cases like k is the middle node then no need to swap
-        if(curr1==curr2) return head;
-
-
-
-        // Swapping Values of both indexes
-       int t=curr1.val;
-       curr1.val=curr2.val;
-       curr2.val=t;
-        
-       
+        int t=slow.val;
+        slow.val=f.val;
+        f.val=t;
         return head;
     }
 }
