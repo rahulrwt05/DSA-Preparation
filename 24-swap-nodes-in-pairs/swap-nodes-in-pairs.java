a92fa06a14885876061df5fbbf29f28ef3c0a78e
Rahul Singh Rawat <125528268+rahulrwt05@users.noreply.github.com>
2024-06-29 12:30:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (27.74%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -10,49 +10,41 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-           ListNode temp=head;
-           int k=2;
-           if(head==null || head.next==null ){
+        ListNode temp=head;
+        if(head==null || head.next==null ){
             return head;
-           }
+        }int k=2;
+
         ListNode knode=findknode(temp,k);
-            head=knode;
-            ListNode lnode=knode;
-            while(temp!=null)
-            {
-              if(knode!=null)
-              {
- ListNode knext=knode.next;
- knode.next=null;
- rev(temp);
- lnode=temp;
- temp=knext;
- 
- 
- 
-              }
-              else{
+        head=knode;
+        ListNode knext=null;
+        ListNode lnode=null;
+        while(temp!=null){
+      if(knode!=null){    knext=knode.next;  knode.next=null;
+            rev(temp);
+            lnode=temp;
+temp=knext;
+        }else{
                 lnode.next=temp;
                 break;
-              }knode=findknode(temp,k);lnode.next=knode;
-}
-return head;
-    }static ListNode findknode(ListNode head, int k){
-        while(head!=null &&  k>1){
-           k--;
+              }
+knode=findknode(temp,k);
+lnode.next=knode;
+        
+        }
+    return head;}static ListNode  findknode(ListNode head,int k){
+        while(head!=null && k>1){
+            k--;
             head=head.next;
         }
-   return head; 
-   }
-   static ListNode rev(ListNode head){
-    ListNode prev=null;
+    return head;}
+static ListNode rev(ListNode head){
     ListNode curr=head;
+    ListNode prev=null;
     while(curr!=null){
         ListNode next=curr.next;
         curr.next=prev;
         prev=curr;
         curr=next;
     }
-    return prev;
-   }
-}
+return prev;}}
